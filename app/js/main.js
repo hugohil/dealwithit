eebc9f618109ffc76d8e1f0b8b849ca7b258d63e
hugo <hilairehugo@gmail.com>
2014-09-21 20:15:10 +0200

    tracking is and webcam displays on canvas + image can be saved

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,37 @@
 window.onload = function() {
+
     var video = document.getElementById('video');
+    var img = document.getElementById('img');
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
 
-    var img = new Image();
-    img.src = '../img/glasses.png';
+    if (navigator.getUserMedia)
+        navigator.getUserMedia({video: true}, handleVideo, videoError);
+
+    function handleVideo(stream){
+        video.src = window.URL.createObjectURL(stream);
+    }
+
+    function videoError(){
+        console.log('No webcam found ... sorry :(');
+    }
+
+    var glasses = new Image();
+    glasses.src = '../img/glasses.png';
 
     var tracker = new tracking.ObjectTracker('face');
     tracker.setInitialScale(4);
     tracker.setStepSize(2);
     tracker.setEdgesDensity(0.1);
 
-    tracking.track('#video', tracker, { camera: true });
+    window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
     tracker.on('track', function(event) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        var ratio = img.width / img.height;
-
         event.data.forEach(function(rect) {
             if(event.data.length == 1){ // make sure just one image is drawn
-                ctx.drawImage(img, rect.x, rect.y + (rect.height / 3), rect.width, rect.width / ratio );
+                
+                ctx.drawImage(glasses, rect.x, rect.y + (rect.height / 3), rect.width, rect.width / (glasses.width / glasses.height));
+                
                 ctx.font = "bold 46px Arial";
                 ctx.strokeStyle = '#000';
                 ctx.lineWidth = 5;
@@ -27,6 +39,17 @@ window.onload = function() {
                 ctx.fillStyle = '#FFF';
                 ctx.fillText('DEAL WITH IT', 50, 50);
             }
+            img.src = canvas.toDataURL();
         });
+    
     });
+
+    function play(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        tracking.track('#canvas', tracker);
+    
+        requestAnimationFrame(play);
+    }    
+    requestAnimationFrame(play);
 };
